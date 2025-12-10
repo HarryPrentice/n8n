8318bc067cd794a23fc4cb4e8db622622d5f627e
autologie <suguru@n8n.io>
2025-12-10 09:17:07 +0100

    feat: add icon to personal and workflow agent

--- packages/@n8n/db/src/migrations/mysqldb/index.ts
+++ packages/@n8n/db/src/migrations/mysqldb/index.ts
@@ -123,6 +123,7 @@ import { CreateWorkflowPublishHistoryTable1764167920585 } from '../common/176416
 import { AddCreatorIdToProjectTable1764276827837 } from '../common/1764276827837-AddCreatorIdToProjectTable';
 import { CreateDynamicCredentialResolverTable1764682447000 } from '../common/1764682447000-CreateCredentialResolverTable';
 import { AddDynamicCredentialEntryTable1764689388394 } from '../common/1764689388394-AddDynamicCredentialEntryTable';
+import { AddIconToAgentTable1765361177092 } from '../common/1765361177092-AddIconToAgentTable';
 import type { Migration } from '../migration-types';
 
 export const mysqlMigrations: Migration[] = [
@@ -251,4 +252,5 @@ export const mysqlMigrations: Migration[] = [
 	AddCreatorIdToProjectTable1764276827837,
 	CreateDynamicCredentialResolverTable1764682447000,
 	AddDynamicCredentialEntryTable1764689388394,
+	AddIconToAgentTable1765361177092,
 ];
